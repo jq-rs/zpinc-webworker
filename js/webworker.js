03a6eacc9e0241178d9f5adfce21514741132b25
ZpincDev <63105467+ZpincDev@users.noreply.github.com>
2022-02-01 21:14:48 +0200

    Update webworker.js

--- js/webworker.js
+++ js/webworker.js
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  * Copyright (c) 2019-2020 MlesTalk WebWorker developers
- * Copyright (c) 2020 Zpinc developers
+ * Copyright (c) 2020-2022 Zpinc developers
  */
 
 
